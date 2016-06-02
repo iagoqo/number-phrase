ea88866b1cab589c4b5d626b522a0200cce1e2fa
Iago <iagoqo@users.noreply.github.com>
2016-06-02 22:52:36 +0200

    Add form validation

--- src/app/services/number.service.js
+++ src/app/services/number.service.js
@@ -13,11 +13,11 @@
     /////
 
     function toPhrase(number) {
-      var ones = ones2Text(number);
-      var tens = tens2Text(number);
-      var teens = teens2Text(number);
-      var hundreds = hundreds2Text(number);
-      var thousands = thousands2Text(number);
+      var ones = onesToText(number);
+      var tens = tensToText(number);
+      var teens = teensToText(number);
+      var hundreds = hundredsToText(number);
+      var thousands = thousandsToText(number);
 
       var and = (thousands || hundreds) && (tens || ones) && ' and ';
       var dash = tens && ones && '-';
@@ -27,14 +27,14 @@
       return phrase.trim() || 'zero';
     }
 
-    function ones2Text(number) {
+    function onesToText(number) {
       var ones = number % 10;
       var onesList = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
       return onesList[ones];
     }
 
-    function teens2Text(number) {
+    function teensToText(number) {
       var twoDigits = number % 100;
       var isTeen = twoDigits > 10 && twoDigits < 20;
       var ones = number%10;
@@ -43,28 +43,28 @@
       return isTeen && teensList[ones];
     }
 
-    function tens2Text(number) {
+    function tensToText(number) {
       var tens = Math.floor(number / 10) % 10;
       var tensList = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
       return tensList[tens] || '';
     }
 
-    function hundreds2Text(number) {
+    function hundredsToText(number) {
       var hundreds = Math.floor(number / 100) % 10;
 
       if (hundreds) {
-        return ones2Text(hundreds) + ' hundred';
+        return onesToText(hundreds) + ' hundred';
       } else {
         return '';
       }
     }
 
-    function thousands2Text(number) {
+    function thousandsToText(number) {
       var thousands = Math.floor(number / 1000) % 10;
 
       if (thousands) {
-        return ones2Text(thousands) + ' thousand';
+        return onesToText(thousands) + ' thousand';
       } else {
         return '';
       }
